16b8f565bdac3db9e5648aec620db4895c556570
Puru12 <zhouyuncong@gmail.com>
2017-09-04 09:07:17 -0400

    Add router
    Add router
    Change card title link based on different card type

--- src/cards.js
+++ src/cards.js
@@ -7,30 +7,63 @@ import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import 'typeface-roboto';
 import renderHTML from 'react-render-html';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function SimpleCard(props) {
   if (props === undefined ){
     return null
   }
+  let titleClick;
+  const linking  = `/post/${props._id}`
+  if (props.url) {
+    titleClick = renderHTML(`<a href="${props['url']}">${props['title']}</a>` );
+  } else {
+    // titleClick = props['title'];
+    // titleClick = renderHTML(`<a href="${linking}">${props['title']}</a>` );
+    titleClick = renderHTML(`<a href="/about">${props['title']}</a>` );
+  };
+
+  const About = () => (
+    <div>
+      <h2>About</h2>
+    </div>
+  )
+  const Home = () => (
+    <div>
+      <h2>Home</h2>
+    </div>
+  )
+
   return (
     <div  key={props['_id']}>
       <Card>
-        <CardHeader title={renderHTML(`<a href="${props['url']}">${props['title']}</a>` )}/>
+        <CardHeader title={titleClick} />
         <CardContent>
           <Typography type="body2" >
             {props['body']}
           </Typography>
+          {/* <Router>
+        <Link to={`/post/${props._id}`}>Home</Link>
+    <Route exact path="/" component={Home}/>
+    <Route path={linking} component={About}/>
+    </Router> */}
           <Typography component="p">
            submitted on {props['timestamp']}
           </Typography>
         </CardContent>
         <CardActions>
-          <Button dense>{props['child_ids'].length} comments</Button>
+          {/* <Button dense>{props['child_ids'].length} comments</Button> */}
         </CardActions>
       </Card>
     </div>
   );
 }
+
+
 
 SimpleCard.propTypes = {
   classes: PropTypes.object.isRequired,
--- src/index.js
+++ src/index.js
@@ -5,26 +5,113 @@ import BasicTabs from './tabs.js'
 import FloatingActionButtons from './mainbutton.js'
 import SimpleDialogDemo from './dialog.js'
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import purple from 'material-ui/colors/purple';
-import green from 'material-ui/colors/green';
-import red from 'material-ui/colors/red';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import SimpleCard from './cards.js'
+import Tabs, { Tab } from 'material-ui/Tabs';
+import PaperSheet from './paper.js'
+
+// using CommonJS modules
+// var Router = require('react-router').Router
+// var Route = require('react-router').Route
+// var Switch = require('react-router').Switch
+// var Link = require('react-router').Link
 
 const theme = createMuiTheme();
 
+const Home = () => (
+  <div>
+         <SimpleDialogDemo />
+       <BasicTabs />
+  </div>
+)
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
 
+const fetchPost = async( id ) =>{
+  const url = `http://localhost:4000/comments/${id}`
+  console.log(url)
+  const response = await fetch(url);
+  const posts = await response.json();
+  return posts;
+}
+
+const getPost = ( {match} ) =>{
+  // const url = `http://localhost:4000/post${match.params.id}`
+  // const response = await fetch(url);
+  // const posts = await response.json();
+  const posts =  fetchPost(match.params.id)
+  console.log(posts)
+
+  // let postPromise = fetchPost(match.params.id);
+  // let comments = []
+  // comments = postPromise.then( (posts) => {
+  //   console.log(posts)
+    // comments = posts
+    // return posts
+    // return (
+    //   <div>
+    //     {posts.map(SimpleCard)}
+    //   </div>
+    // )
+  // })
+  // console.log(comments)
+  // render() ;{
+  //   const classes = this.props.classes;
+  //   if (posts === undefined) {
+  //     return null
+  //   } else {
+
+      return (
+        <div>
+          {/* <PaperSheet /> */}
+          {/* <SimpleCard name={posts[0]} /> */}
+          {/* {posts.map(SimpleCard)} */}
+          <h2>hello {match.params.id}</h2>
+          {/* {comments.map(SimpleCard)} */}
+          {/* <BasicTabs /> */}
+        </div>
+      )
+    // }
+    // { (  ) => <div>{match}</div>}
+  // }
+}
 const App = () => {
   return (
-      <MuiThemeProvider theme={theme}>
+        <Router>
     <div>
-      <SimpleDialogDemo>
-        </SimpleDialogDemo>
-       <BasicTabs>
-      </BasicTabs>
+    {/* <Link to="/">Home</Link>
+    <Link to="/about">about</Link> */}
+    <AppBar position="static">
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            @chan
+          </Typography>
+        </Toolbar>
+        </AppBar>
+         <Route exact path="/" component={Home}/>
+         <Route path="/about" component={About}/>
+         <Route path="/comments/:id" component={getPost} />
+      {/* <SimpleDialogDem />
+       <BasicTabs /> */}
+
       {/* <FloatingActionButtons>
       </FloatingActionButtons> */}
     </div>
-      </MuiThemeProvider>
+    </Router>
   );
 }
+    //   <Link to="/test">Home</Link>
+      //       <MuiThemeProvider theme={theme}>
+      // </MuiThemeProvider>
 
 render(<App />, document.querySelector('#root'));
